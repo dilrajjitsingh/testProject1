6e8c500cbb34b4e88fdecdbfa16df77b7f1e722d
Dilraj Jit Singh Khurl <dilrajjitsingh@gmail.com>
2021-11-11 09:01:02 +0530

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testProject1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Console World!");
+        }
+    }
+}
